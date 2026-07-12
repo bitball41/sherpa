1da1306a2c651930dc89b7b30bae4296325cc2eb
Conno <nissimconnor@gmail.com>
2026-07-12 12:53:14 -0700

    bind Playwright server to loopback

--- playwright.config.ts
+++ playwright.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "pnpm run dev",
+		command: "HOST=127.0.0.1 pnpm run dev",
 		url: "http://127.0.0.1:1337",
 		reuseExistingServer: false,
 	},
