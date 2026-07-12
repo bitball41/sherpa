2112a97af81af911f00a9eee44708f0af6343152
Conno <nissimconnor@gmail.com>
2026-07-12 12:55:14 -0700

    virtualize IndexedDB metadata and deletion

--- src/client/shared/indexeddb.ts
+++ src/client/shared/indexeddb.ts
@@ -1,9 +1,34 @@
 import { SherpaClient } from "@client/index";
+import { storagePrefix, unprefixStorageKey } from "@/shared/storage";
 
 export default function (client: SherpaClient) {
+	const prefix = storagePrefix(client.url.origin);
+
 	client.Proxy("IDBFactory.prototype.open", {
 		apply(ctx) {
-			ctx.args[0] = `${client.url.origin}@${ctx.args[0]}`;
+			ctx.args[0] = prefix + ctx.args[0];
+		},
+	});
+
+	client.Proxy("IDBFactory.prototype.deleteDatabase", {
+		apply(ctx) {
+			ctx.args[0] = prefix + ctx.args[0];
+		},
+	});
+
+	client.Proxy("IDBFactory.prototype.databases", {
+		apply(ctx) {
+			const result = ctx.call() as Promise<IDBDatabaseInfo[]>;
+			ctx.return(
+				result.then((databases) =>
+					databases
+						.filter((database) => database.name?.startsWith(prefix))
+						.map((database) => ({
+							...database,
+							name: unprefixStorageKey(database.name, client.url.origin),
+						}))
+				)
+			);
 		},
 	});
 
@@ -11,7 +36,9 @@ export default function (client: SherpaClient) {
 		get(ctx) {
 			const name = ctx.get() as string;
 
-			return name.substring(name.indexOf("@") + 1);
+			return name.startsWith(prefix)
+				? unprefixStorageKey(name, client.url.origin)
+				: name;
 		},
 	});
 }
