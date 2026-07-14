c8829e0b151b66348992d59a771da415fb747f4a
Conno <nissimconnor@gmail.com>
2026-07-13 17:41:59 -0700

    Make static build script fail safely

--- ci/build-static.sh
+++ ci/build-static.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-DST=staticbuild
+set -euo pipefail
 
-rm -rf $DST
-mkdir -p $DST
+DST=${DST:-staticbuild}
 
-cp -r node_modules/@mercuryworkshop/bare-mux/dist/ $DST/baremux
-cp -r node_modules/@mercuryworkshop/epoxy-transport/dist/ $DST/epoxy
-cp -r node_modules/@mercuryworkshop/libcurl-transport/dist/ $DST/libcurl
-cp -r assets/ $DST/assets
-cp -r dist/ $DST/scram
-cp -r static/* $DST
+rm -rf "$DST"
+mkdir -p 	"$DST/baremux" 	"$DST/epoxy" 	"$DST/libcurl" 	"$DST/assets" 	"$DST/scram"
 
-if [ -d "_docs" ]; then
-    cp -r _docs/ $DST/typedoc
+cp -a node_modules/@mercuryworkshop/bare-mux/dist/. "$DST/baremux/"
+cp -a node_modules/@mercuryworkshop/epoxy-transport/dist/. "$DST/epoxy/"
+cp -a node_modules/@mercuryworkshop/libcurl-transport/dist/. "$DST/libcurl/"
+cp -a assets/. "$DST/assets/"
+cp -a dist/. "$DST/scram/"
+cp -a static/. "$DST/"
+
+if [[ -d _docs ]]; then
+	mkdir -p "$DST/typedoc"
+	cp -a _docs/. "$DST/typedoc/"
 fi
-if [ -d "_docs-dev" ]; then
-    cp -r _docs-dev/ $DST/typedoc-dev
+if [[ -d _docs-dev ]]; then
+	mkdir -p "$DST/typedoc-dev"
+	cp -a _docs-dev/. "$DST/typedoc-dev/"
 fi
 
-printf 'let _CONFIG = %s' "$(jq -c -n '{"wispurl": "wss://anura.pro/", "bareurl": "https://aluu.xyz/bare/"}')" > $DST/config.js
+printf 'let _CONFIG = %s' 	"$(jq -c -n '{"wispurl": "wss://anura.pro/", "bareurl": "https://aluu.xyz/bare/"}')" 	> "$DST/config.js"
+
