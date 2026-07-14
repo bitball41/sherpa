85c14a6bf9b3aa90ff69727e96f058b2edc1bfbf
Conno <nissimconnor@gmail.com>
2026-07-14 16:33:07 -0700

    fix: include ambient declarations required by published types

---
+++ dist/types/global.d.ts
@@ -0,0 +1,12 @@
+declare const dbg: {
+	log: (message: string, ...args: any[]) => void;
+	warn: (message: string, ...args: any[]) => void;
+	error: (message: string, ...args: any[]) => void;
+	debug: (message: string, ...args: any[]) => void;
+	time: (meta: URLMeta, before: number, type: string) => void;
+};
+
+declare const COMMITHASH: string;
+declare const VERSION: string;
+
+declare type Self = Window & typeof globalThis;
