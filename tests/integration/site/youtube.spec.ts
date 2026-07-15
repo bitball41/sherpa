41fff3288104c623e56e9a4a14240e739cb32cbe
Conno <nissimconnor@gmail.com>
2026-07-14 18:26:55 -0700

    test: make YouTube checks explicitly opt in

--- tests/integration/site/youtube.spec.ts
+++ tests/integration/site/youtube.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from "@playwright/test";
 import { setupPage } from "../../util/setupPage";
 
-test.describe("YouTube", () => {
+const RUN_LIVE_SITE_TESTS = process.env.LIVE_SITE_TESTS === "1";
+
+test.describe("YouTube (live)", () => {
+	test.beforeEach(() => {
+		test.skip(
+			!RUN_LIVE_SITE_TESTS,
+			"Set LIVE_SITE_TESTS=1 to run checks that depend on YouTube"
+		);
+	});
 	test("The front page can load.", async ({ page }) => {
 		const frame = await setupPage(page, "https://www.youtube.com/");
 
