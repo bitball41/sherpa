74636ade7701d68720c6e660af6113eee2cc1ecb
Conno <nissimconnor@gmail.com>
2026-07-14 19:06:18 -0700

    fix(types): repair generated rewriter declaration

--- dist/types/shared/rewriters/wasm.d.ts
+++ dist/types/shared/rewriters/wasm.d.ts
@@ -1,7 +1,15 @@
-import { Rewriter } from "../../../rewriter/wasm/out/wasm.js";
-import type { JsRewriterOutput } from "../../../rewriter/wasm/out/wasm.js";
-export type { JsRewriterOutput, Rewriter };
 import { URLMeta } from "./url";
+export type JsRewriterOutput = {
+    js: Uint8Array;
+    map: Uint8Array;
+    scramtag: string;
+    errors: string[];
+};
+export interface Rewriter {
+    rewrite_js(js: string, base: string, url: string, module: boolean): JsRewriterOutput;
+    rewrite_js_bytes(js: Uint8Array, base: string, url: string, module: boolean): JsRewriterOutput;
+    free(): void;
+}
 export declare function asyncSetWasm(): Promise<void>;
 export declare const textDecoder: TextDecoder;
 export declare function getRewriter(meta: URLMeta): [Rewriter, () => void];
