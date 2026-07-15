168c54784be4689f2f2c0a818e0e9950b4fb4a18
Conno <nissimconnor@gmail.com>
2026-07-14 18:38:53 -0700

    fix: initialize configured demo transports correctly

--- static/ui.js
+++ static/ui.js
@@ -20,7 +20,7 @@ const runtimeReady = Promise.all([
 	navigator.serviceWorker
 		.register("./sw.js")
 		.then(() => navigator.serviceWorker.ready),
-	connection.setTransport(store.transport, [{ wisp: store.wispurl }]),
+	connection.setTransport(store.transport, sherpaTransportOptions(store.transport)),
 ]);
 const flex = css`
 	display: flex;
